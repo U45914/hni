fb6d24ccd33bc078e2404cf1b1e2f5485d629551
somdas <U47592@ust-global.com>
2017-04-04 19:22:23 +0530

    Created HniServiceDto and its associated converter and retrieved data from database

--- hni-organization/src/main/java/org/hni/organization/dao/DefaultUserOrganizationRoleDAO.java
+++ hni-organization/src/main/java/org/hni/organization/dao/DefaultUserOrganizationRoleDAO.java
@@ -2,7 +2,6 @@ package org.hni.organization.dao;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
@@ -11,6 +10,7 @@ import org.hni.common.dao.AbstractDAO;
 import org.hni.common.om.Role;
 import org.hni.organization.om.Organization;
 import org.hni.organization.om.UserOrganizationRole;
+import org.hni.organization.om.HniServices;
 import org.hni.user.om.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -52,6 +52,17 @@ public class DefaultUserOrganizationRoleDAO extends AbstractDAO<UserOrganization
 		try {
 			Query q = em.createQuery("SELECT x FROM UserOrganizationRole x WHERE x.id.roleId = :roleId")
 				.setParameter("roleId", role.getId());
+			return q.getResultList();
+		} catch(NoResultException e) {
+			return Collections.emptyList();
+		}
+	}
+	
+	public Collection<HniServices> getHniServicesByRole(Long orgId, Long roleId) {
+		try {
+			Query q = em.createQuery("SELECT x FROM HniServices x WHERE x.orgId = :orgId AND x.roleId = :roleId")
+				.setParameter("orgId", orgId)
+				.setParameter("roleId", roleId);
 			return q.getResultList();
 		} catch(NoResultException e) {
 			return Collections.emptyList();
