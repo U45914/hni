e073e5c539b6a01f267241cf646faa08e9499807
U44592 <U44592@ust-global.com>
2017-04-10 11:03:08 +0530

    User Authentication Error Fixed

--- hni-security/src/main/java/org/hni/security/utils/HNISecurityUtils.java
+++ hni-security/src/main/java/org/hni/security/utils/HNISecurityUtils.java
@@ -3,16 +3,21 @@ package org.hni.security.utils;
 import java.security.SecureRandom;
 import java.util.Base64;
 
+import org.apache.shiro.authc.SaltedAuthenticationInfo;
 import org.apache.shiro.crypto.hash.Hash;
-import org.apache.shiro.crypto.hash.Sha256Hash;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
+import org.apache.shiro.util.SimpleByteSource;
 
 
 
 public class HNISecurityUtils {
 
-	public static String getHash(String authCode, byte[] salt) {
-		Hash h = new Sha256Hash(authCode, salt, 1000);
-		return h.toHex();
+	public static String getHash(String authCode, Object salt) {
+		ByteSource slt = new SimpleByteSource(org.apache.commons.codec.binary.Base64.decodeBase64((String) salt));
+		Hash h = new SimpleHash("SHA-256", authCode, slt, 1024);
+		Base64.Encoder enc = Base64.getEncoder();
+		return new String(enc.encode(h.getBytes()));
 	}
 
 	public static String getSalt() {
