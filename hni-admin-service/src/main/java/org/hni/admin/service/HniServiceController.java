0fe84f0e15056327d85c7658d999ca26007da518
somdas <U47592@ust-global.com>
2017-06-15 18:50:48 +0530

    Included method api to retrieve client faq and volunteer guide

--- hni-admin-service/src/main/java/org/hni/admin/service/HniServiceController.java
+++ hni-admin-service/src/main/java/org/hni/admin/service/HniServiceController.java
@@ -5,6 +5,9 @@ package org.hni.admin.service;
 
 import java.io.File;
 
+import javax.activation.DataSource;
+import javax.activation.URLDataSource;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
@@ -26,16 +29,45 @@ import io.swagger.annotations.SwaggerDefinition;
 @Path("/help")
 public class HniServiceController extends AbstractBaseController {
 
-	private static final String FILE_PATH_VOLUNTEER_INSTRUCTION = "instructions/HNI _Volunteer_Guide_Meal_Orders.pdf";
+	private static final String FILE_PATH_VOLUNTEER_INSTRUCTION = "instructions/HNI_Volunteer_Guide_Meal_Orders.pdf";
 	private static final String VOLUNTEER_INSTRUCTION_PDF = "volunteer-guide.pdf";
+	private static final String FILE_PATH_CLIENT_FAQ = "instructions/HNI_FAQ.pdf";
+	private static final String CLIENT_FAQ_PDF = "HungerNotImpossibleFrequentlyAskedQuestions.pdf";
 
+	@GET
 	@Path("/volunteer/instruction/pdf")
 	@Produces("application/pdf")
 	public Response getVolunteerInstructionPdf() {
 		try {
-		File file = new File(FILE_PATH_VOLUNTEER_INSTRUCTION);
+			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+	        if (classLoader == null) {
+	            classLoader = HniServiceController.class.getClassLoader();
+	        }
+	        DataSource footerDs = new URLDataSource(classLoader.getResource(FILE_PATH_VOLUNTEER_INSTRUCTION));
+		File file = new File(footerDs.getName());
 			ResponseBuilder response = Response.ok((Object) file);
 			response.header("Content-Disposition", "attachment; filename=\" " + VOLUNTEER_INSTRUCTION_PDF + "\"");
+			return response.build();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return Response.noContent().build();
+	}
+	
+	@GET
+	@Path("/client/faq/pdf")
+	@Produces("application/pdf")
+	public Response getClientFAQPdf() {
+		try {
+			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+	        if (classLoader == null) {
+	            classLoader = HniServiceController.class.getClassLoader();
+	        }
+	        DataSource footerDs = new URLDataSource(classLoader.getResource(FILE_PATH_CLIENT_FAQ));
+		File file = new File(footerDs.getName());
+			ResponseBuilder response = Response.ok((Object) file);
+			response.header("Content-Disposition", "inline; filename=\" " + CLIENT_FAQ_PDF + "\"");
 			return response.build();
 		} catch (Exception e) {
 			e.printStackTrace();
